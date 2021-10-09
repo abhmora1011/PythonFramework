1fae7ebe3c8cd0b54b24c1bd9d165b75d0f1fd41
abeora <abraha.morial.ora@gmail.com>
2021-10-09 12:48:32 +0800

    Implementing Data Driven Test

--- tests/test_HomePage.py
+++ tests/test_HomePage.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.support.select import Select
 
 from page_objects.homepage import HomePage
@@ -7,17 +8,17 @@ from utilities.data_bank import TestData
 
 class Test_HomePage(BaseClass):
 
-    def test_formSubmission(self):
+    def test_formSubmission(self, get_data):
 
         homePage = HomePage(self.driver)
 
-        homePage.name_field().send_keys(TestData.NAME)
+        homePage.name_field().send_keys(get_data[0])
 
-        homePage.email_field().send_keys(TestData.EMAIL)
+        homePage.email_field().send_keys(get_data[1])
 
         homePage.password_field().send_keys(TestData.PASSWORD)
 
-        self.selectOptionByText(homePage.gender_field(), TestData.GENDER)
+        self.selectOptionByText(homePage.gender_field(), get_data[2])
 
         homePage.check_ice_cream().click()
 
@@ -26,3 +27,9 @@ class Test_HomePage(BaseClass):
         message = homePage.success_text().text
 
         assert "Success" in message
+
+        self.driver.refresh()
+
+    @pytest.fixture(params=[("Abraham Ora", "abraham.morial.ora@gmail.com", "Male"), ("Clarisse Ora", "clarisse.licuanan.ora@gmail.com", "Female") ])
+    def get_data(self, request):
+        return request.param
